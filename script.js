784a8f918dacb78ebab74e4f959f417bac2cca0f
Nikhil Sharma <nikhil@Nikhils-MacBook-Pro.local>
2023-07-18 19:02:51 +0530

    Given a basic structure and the add book functionality. What's left is showing up the book

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+    console.log("sdshdjsnd")
+  document.getElementById("book").showModal();
+}
+
